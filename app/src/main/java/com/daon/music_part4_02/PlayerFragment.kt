a89e7d377525cf5d3a00b5753af13c0f403e80d3
daon1213 <diaeiyo1213@naver.com>
2022-04-18 20:00:02 +0900

    Player Fragment commit!

--- app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
+++ app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
@@ -3,12 +3,42 @@ package com.daon.music_part4_02
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.daon.music_part4_02.service.MusicDto
+import com.daon.music_part4_02.service.MusicService
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class PlayerFragment: Fragment(R.layout.fragment_player) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getVideoListFromServer()
+    }
+
+    private fun getVideoListFromServer() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(MusicService::class.java)
+            .also {
+                it.listMusics()
+                    .enqueue(object: Callback<MusicDto>{
+                        override fun onResponse(
+                            call: Call<MusicDto>,
+                            response: Response<MusicDto>,
+                        ) {
+
+                        }
+
+                        override fun onFailure(call: Call<MusicDto>, t: Throwable) {
+
+                        }
+
+                    })
+            }
     }
 
     companion object {
