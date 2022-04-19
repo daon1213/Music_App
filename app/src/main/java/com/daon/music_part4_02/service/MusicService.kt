e1b22d1e1d0968e76fb535a8dc80b12f7b7ae7e9
daon1213 <diaeiyo1213@naver.com>
2022-04-19 16:16:04 +0900

    Music Service commit!

--- app/src/main/java/com/daon/music_part4_02/service/MusicService.kt
+++ app/src/main/java/com/daon/music_part4_02/service/MusicService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 interface MusicService {
 
     @GET("/v3/6dbd937e-a2fa-4e28-a98a-f3bbaf847f1a")
-    fun listMusics() : Call<MusicDto>
+    fun getListMusics() : Call<MusicDto>
 }
