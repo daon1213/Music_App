fa350aa831ca8f4d059b46ec64bbde8c6e9292ec
daon1213 <diaeiyo1213@naver.com>
2022-04-19 14:58:13 +0900

    PlayerFragment commit!

--- app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
+++ app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
@@ -2,18 +2,38 @@ package com.daon.music_part4_02
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.daon.music_part4_02.databinding.FragmentPlayerBinding
 import com.daon.music_part4_02.service.MusicDto
 import com.daon.music_part4_02.service.MusicService
+import com.daon.music_part4_02.service.mapper
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class PlayerFragment: Fragment(R.layout.fragment_player) {
 
+    private var binding: FragmentPlayerBinding? = null
+    private var isWatchingPlayListView = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
+        binding = fragmentPlayerBinding
+
+        initPlayListButton(fragmentPlayerBinding)
+
         getVideoListFromServer()
+    }
+
+    private fun initPlayListButton(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playlistImageView.setOnClickListener {
+            fragmentPlayerBinding.playerViewGroup.isVisible = isWatchingPlayListView
+            fragmentPlayerBinding.playListViewGroup.isVisible = isWatchingPlayListView.not()
+
+            isWatchingPlayListView = !isWatchingPlayListView
+        }
     }
 
     private fun getVideoListFromServer() {
@@ -30,7 +50,11 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
                             call: Call<MusicDto>,
                             response: Response<MusicDto>,
                         ) {
-
+                            response.body()?.let {
+                                val modelList = it.musics.mapIndexed { index, musicEntity ->
+                                    musicEntity.mapper(index.toLong())
+                                }
+                            }
                         }
 
                         override fun onFailure(call: Call<MusicDto>, t: Throwable) {
