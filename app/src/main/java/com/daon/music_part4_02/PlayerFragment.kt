d7802db6fd839d73b03abf989c46009dffffafd8
daon1213 <diaeiyo1213@naver.com>
2022-04-19 16:16:38 +0900

    Player Fragment commit!

--- app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
+++ app/src/main/java/com/daon/music_part4_02/PlayerFragment.kt
@@ -1,20 +1,32 @@
 package com.daon.music_part4_02
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.daon.music_part4_02.databinding.FragmentPlayerBinding
-import com.daon.music_part4_02.service.MusicDto
-import com.daon.music_part4_02.service.MusicService
-import com.daon.music_part4_02.service.mapper
+import com.daon.music_part4_02.service.*
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-class PlayerFragment: Fragment(R.layout.fragment_player) {
+class PlayerFragment : Fragment(R.layout.fragment_player) {
 
+    private var model: PlayerModel = PlayerModel()
     private var binding: FragmentPlayerBinding? = null
-    private var isWatchingPlayListView = true
+    private lateinit var playListAdapter: PlayListAdapter
+    private var player: SimpleExoPlayer? = null
+
+    private val updateSeekRunnable = Runnable {
+        updateSeek()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,47 +34,215 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
         binding = fragmentPlayerBinding
 
+        initPlayView(fragmentPlayerBinding)
         initPlayListButton(fragmentPlayerBinding)
+        initPlayControlButtons(fragmentPlayerBinding)
+        initSeekBar(fragmentPlayerBinding)
+        initRecyclerView(fragmentPlayerBinding)
 
-        getVideoListFromServer()
+        getMusicListFromServer()
+    }
+
+    private fun initSeekBar(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playerSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {}
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                player?.seekTo((seekBar.progress * 1000).toLong()) // ms 로 처리 필요
+            }
+        })
+
+        fragmentPlayerBinding.playListSeekbar.setOnTouchListener { view, motionEvent -> false }
+    }
+
+    private fun initRecyclerView(fragmentPlayerBinding: FragmentPlayerBinding) {
+        playListAdapter = PlayListAdapter {
+            // TODO 음악 재생
+            playMusic(it)
+        }
+        fragmentPlayerBinding.playListRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentPlayerBinding.playListRecyclerView.adapter = playListAdapter
     }
 
     private fun initPlayListButton(fragmentPlayerBinding: FragmentPlayerBinding) {
         fragmentPlayerBinding.playlistImageView.setOnClickListener {
-            fragmentPlayerBinding.playerViewGroup.isVisible = isWatchingPlayListView
-            fragmentPlayerBinding.playListViewGroup.isVisible = isWatchingPlayListView.not()
-
-            isWatchingPlayListView = !isWatchingPlayListView
+            // TODO 만약 서버에서 데이터가 다 불러오지 않은 상태인 경우
+            if (model.currentPosition == -1) return@setOnClickListener
+            fragmentPlayerBinding.playerViewGroup.isVisible = model.isWatchingPlayListView
+            fragmentPlayerBinding.playListViewGroup.isVisible = !model.isWatchingPlayListView
+            model.isWatchingPlayListView = !model.isWatchingPlayListView
         }
     }
 
-    private fun getVideoListFromServer() {
+    private fun initPlayView(fragmentPlayerBinding: FragmentPlayerBinding) {
+        context?.let {
+            player = SimpleExoPlayer.Builder(it).build()
+        }
+        fragmentPlayerBinding.playerView.player = player
+        binding?.let { binding ->
+            player?.addListener(object : Player.EventListener {
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+
+                    if (isPlaying) { // 재생중
+                        binding.playControlImageView.setImageResource(R.drawable.ic_baseline_pause_48)
+                    } else {
+                        binding.playControlImageView.setImageResource(R.drawable.ic_baseline_play_arrow_48)
+                    }
+                }
+
+                // 재생중인 Media 가 변경되었을 때
+                override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                    super.onMediaItemTransition(mediaItem, reason)
+
+                    val newIndex = mediaItem?.mediaId ?: return
+                    model.currentPosition = newIndex.toInt()
+
+                    // playView 업데이트
+                    updatePlayerView(model.currentMusicModel())
+                    playListAdapter.submitList(model.getAdapterModels())
+                }
+
+                // player 의 상태가 바뀔 때
+                override fun onPlaybackStateChanged(playbackState: Int) {
+                    super.onPlaybackStateChanged(playbackState)
+                    updateSeek()
+                }
+            })
+        }
+    }
+
+    private fun updateSeek() {
+        val player = this.player ?: return
+        val duration = if (player.duration >= 0) player.duration else return // 현재 재생중인 Media 의 길이
+        val position = player.currentPosition // 현재 재생 위치
+
+        // TODO UI Update
+        updateSeekUi(duration, position)
+
+        // 1초 마다 updateSeek 호출 handler 생성
+        val state = player.playbackState
+
+        view?.removeCallbacks(updateSeekRunnable)
+        if (state != Player.STATE_IDLE && state != Player.STATE_ENDED) {
+            // 현재 재생중
+            view?.postDelayed(updateSeekRunnable, 1000)
+        }
+    }
+
+    private fun updateSeekUi(duration: Long, position: Long) {
+        binding?.let { binding ->
+            // duration : ms
+            binding.playListSeekbar.max = (duration / 1000).toInt()
+            binding.playListSeekbar.progress = (position / 1000).toInt()
+
+            binding.playerSeekBar.max = (duration / 1000).toInt()
+            binding.playerSeekBar.progress = (position / 1000).toInt()
+
+            binding.playTimeTextView.text = "%02d:%02d".format(
+                TimeUnit.MINUTES.convert(position, TimeUnit.MILLISECONDS),
+                (position / 1000) % 60
+            )
+            binding.totalTimeTextView.text = "%02d:%02d".format(
+                TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS),
+                (duration / 1000) % 60
+            )
+        }
+    }
+
+    private fun updatePlayerView(currentMusicModel: MusicModel?) {
+        currentMusicModel ?: return
+
+        // view 업데이트
+        binding?.let { binding ->
+            binding.trackTextView.text = currentMusicModel.track
+            binding.artistTextView.text = currentMusicModel.artist
+            Glide.with(binding.coverImageCardView.context)
+                .load(currentMusicModel.coverUrl)
+                .into(binding.coverImageView)
+        }
+    }
+
+    private fun initPlayControlButtons(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playControlImageView.setOnClickListener {
+            val player = this.player ?: return@setOnClickListener
+
+            if (player.isPlaying) { // 재생중
+                player.pause() // -> 일시정지
+            } else {
+                player.play()
+            }
+        }
+
+        fragmentPlayerBinding.skipNextImageView.setOnClickListener {
+            val nextMusic = model.getNextMusic() ?: return@setOnClickListener
+            playMusic(nextMusic)
+        }
+        fragmentPlayerBinding.skipPrevImageView.setOnClickListener {
+            val prevMusic = model.getPrevMusic() ?: return@setOnClickListener
+            playMusic(prevMusic)
+        }
+    }
+
+    private fun getMusicListFromServer() {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://run.mocky.io")
+            .baseUrl("https://run.mocky.io/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
         retrofit.create(MusicService::class.java)
             .also {
-                it.listMusics()
-                    .enqueue(object: Callback<MusicDto>{
+                it.getListMusics()
+                    .enqueue(object : Callback<MusicDto> {
                         override fun onResponse(
                             call: Call<MusicDto>,
-                            response: Response<MusicDto>,
+                            response: Response<MusicDto>
                         ) {
-                            response.body()?.let {
-                                val modelList = it.musics.mapIndexed { index, musicEntity ->
-                                    musicEntity.mapper(index.toLong())
-                                }
+                            Log.d("PlayerFragment", response.body().toString())
+                            response.body()?.let { musicDto ->
+                                model = musicDto.mapper()
+                                setMusicList(model.getAdapterModels())
+                                playListAdapter.submitList(model.getAdapterModels())
                             }
                         }
 
                         override fun onFailure(call: Call<MusicDto>, t: Throwable) {
-
+                            Log.d("PlayerFragment", t.toString())
                         }
-
                     })
             }
+    }
+
+    private fun setMusicList(musicList: List<MusicModel>) {
+        context?.let {
+            player?.addMediaItems(musicList.map {
+                // MediaItem.fromUri() 도 가능
+                MediaItem.Builder()
+                    .setMediaId(it.id.toString()) // 이후에 미디어 리스트에서 특정 미디어를 찾을 때 사용
+                    .setUri(it.streamUrl).build()
+            })
+            player?.prepare()
+        }
+    }
+
+    private fun playMusic(musicModel: MusicModel) {
+        // player : MediaItem
+        // musicModel
+        model.updateCurrentPosition(musicModel)
+        player?.seekTo(model.currentPosition, 0)
+        player?.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+        view?.removeCallbacks(updateSeekRunnable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+        player?.release()
+        view?.removeCallbacks(updateSeekRunnable)
     }
 
     companion object {
